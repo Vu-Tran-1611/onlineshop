2d4c28f33c29f82b02fbe27fe8d2735b69327d9b
tran my <tranmy@Mac.attwifi.manager>
2026-06-13 11:20:58 -0400

    Fixing Routing

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -678,6 +678,7 @@ class HomeController extends Controller
     // Knowledge Base document by URL slug
     public function knowledgeBaseDocument(string $documentSlug)
     {
+        dd($documentSlug);
         $content = KnowledgeBaseDocument::where("slug", $documentSlug)->first();
         if (!$content) {
             return redirect()->route("not-found");
